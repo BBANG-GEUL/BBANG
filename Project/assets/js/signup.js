5a256cc7f2b09116bb24f9ee463ac9dd52b4c343
BBANG-GEUL <ibangglei@naver.com>
2016-08-09 14:20:37 +1200

    hw-signup update ver 0.1
    homework update ver 0.2
    added url (config.js)
    added object function (signup.js)

--- Project/assets/js/signup.js
+++ Project/assets/js/signup.js
@@ -10,36 +10,57 @@
 //			서버에 전달하는 펑션을 만들어 줘야 하는 거죠 ?
 
 var signup = {
-	doSignup: function(){
-		alert("가입 완료 되었습니다.");
-	},
+	doSignup:function(){
+	// web server
+	//var url = "http://128.199.188.157:7000/api/signup";
+
+	// assign of email, name, password value
+	var email = $('#exampleInputEmail1').val();
+	var name = $('#exampleInputName1').val();
+	var password = $('#exampleInputPassword1').val();
+
+	$.post(config.url+"signup", {email:email, name:name, password:password})
+	.done(function(data){
+		$('.custom_form').css('display', 'none');
+		$('.signup_info p.email').text(data.email);
+		$('.signup_info p.name').text(data.name);
+		$('.signup_info p.password').text(data.password);
+
+		$('#confirmed_form').css('display', 'block');
+		$('#signup_input_form').css('display', 'none');
+
+	}).fail(function(data){
+		$('.custom_form').css('display', 'none');
+		$('#failed_form').css('display', 'block');
+	});
+},
 	setEvent: function(){
 		$('#doSignup').click(signup.doSignup);
 	},
 	init: function(){
 		signup.setEvent();
-	}
+	}	
 }
 
 // 1st: 펑션형 ;기본적인 벨류 변수에 저장 및 성공 실패 결과 
 // ★ 질문 : 서버에 전달이 되었는지를 확인 할수가 없는데 ...
 //			url 에 대한 별도의 세팅이 필요한가요 ?
 //			수업 시간에 보여주신 것처럼 header ... 등 정보 전달을 눈으로 확인하고 싶습니다만.... 
-function signup(){
-	// web server
-	var url = "http://128.199.188.157:7000/api/signup";
-	// assign of email, name, password value
-	var email = $('#exampleInputEmail1').val();
-	var name = $('#exampleInputName1').val();
-	var password = $('#exampleInputPassword1').val();
-
-	$.post(url, {email:email, name:name, password:password})
-	.done(function(){
-		alert(email);
-	})
-	.fail(function(){
-		alert(name);
-	})
-}
+//function signup(){
+//	// web server
+//	var url = "http://128.199.188.157:7000/api/signup";
+//	// assign of email, name, password value
+//	var email = $('#exampleInputEmail1').val();
+//	var name = $('#exampleInputName1').val();
+//	var password = $('#exampleInputPassword1').val();
+//
+//	$.post(url, {email:email, name:name, password:password})
+//	.done(function(){
+//		alert(email);
+//	})
+//	.fail(function(){
+//		alert(name);
+//	})
+//}
 
 
